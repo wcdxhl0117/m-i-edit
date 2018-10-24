e628a947eabf963b844fe0c566fb1b8894df3e05
wangchao <wcd126yx@126.com>
2018-10-24 13:27:09 +0800

    增加右下角字符

--- src/store/index.js
+++ src/store/index.js
@@ -69,7 +69,7 @@ const createStore = () => {
     const defaultKeypadType = KeypadTypes.EXPRESSION;
 
     const initialKeypadState = {
-        extraKeys: ['x', 'y', Keys.THETA, Keys.PI],
+        extraKeys: ['x', 'y','z','f', 'g', Keys.THETA, Keys.PI],
         keypadType: defaultKeypadType,
         active: false,
     };
