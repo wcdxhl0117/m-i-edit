0866a59d7618281802bec05c6f3b6805f34cab6c
wangchao <wcd126yx@126.com>
2018-12-03 13:21:05 +0800

    手写板调整

--- src/components/whiteboard.js
+++ src/components/whiteboard.js
@@ -2,14 +2,30 @@
 
 import React, {Component} from 'react';
 
+const ReactDOM = require('react-dom');
+const MathWrapper = require('./input/math-wrapper');
+
 // import {Sketchable} from '../../whiteboard/sketchable.full.min'
 const {View,Text} = require('../fake-react-native-web');
 var sketcher = null;
-let wi = screen.width - 5;
-console.log(wi)
+let wi = screen.width - 3;
 
 export default class Whiteboard extends Component {
+    
     componentDidMount() {
+        
+        this.mathField1 = new MathWrapper(this._mathContainer, {}, {
+            onCursorMove: (cursor) => {
+                // TODO(charlie): It's not great that there is so much coupling
+                // between this keypad and the input behavior. We should wrap
+                // this `MathInput` component in an intermediary component
+                // that translates accesses on the keypad into vanilla props,
+                // to make this input keypad-agnostic.
+                this.props.keypadElement &&
+                    this.props.keypadElement.setCursor(cursor);
+            },
+        });
+
         // console.log('componentDidMount', this.refs.canvas);
         // let options = {
         //     events: {
@@ -62,10 +78,35 @@ export default class Whiteboard extends Component {
     
     render() {
         // return <div><h5>Hello</h5></div>;
+        let str = '\\sqrt[3]{4}';
         return (<View>
-            <Text onPress={this.generateSVGInk}>{'SVG INK'}</Text>
-            <canvas id="drawing-canvas" width={wi} height="200" ref="canvas"
+            <View
+                ref={(node) => {
+                    this._mathContainer = ReactDOM.findDOMNode(node);
+                }}
+            >
+                <Text
+                    onPress={this.generateSVGInk}
+                >{str}</Text>
+                <Text
+                    onPress={this.generateSVGInk}
+                >{str}</Text>
+                <Text
+                    onPress={this.generateSVGInk}
+                >{str}</Text>
+            </View>
+            <canvas id="drawing-canvas" width={wi} height="220" ref="canvas"
+                    // onTouchEnd={this.generateSVGInk}
                     style={{'border': '1px solid #000000'}}/>
         </View>);
     }
 }
+
+// export default class Pre extends Component {
+//     constructor(props) {
+//         super(props)
+//     }
+//     render() {
+//         <div>{this.props.preVue}</div>
+//     }
+// }
