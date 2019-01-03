5ae4e58940afe9bee2df06e626358e43196c5b1d
wangchao <wcd126yx@126.com>
2019-01-03 15:54:25 +0800

    波浪号

--- src/components/expression-keypad.js
+++ src/components/expression-keypad.js
@@ -656,24 +656,20 @@ class ExpressionKeypad extends React.Component {
                     borders={BorderStyles.NONE}
                 />
             </View>
-            {/* 新增一列，不要再多了，不然格子宽度不够 最多容下6列*/}
+            {/* 新增一列 */}
             <View style={[column, oneColumn]}>
-                {/* 新增一行 */}
                 <TouchableKeypadButton
-                    keyConfig={KeyConfigs.COS}
+                    keyConfig={KeyConfigs.SIM}
                     borders={BorderStyles.NONE}
                 />
-                {/* 新增一行 */}
                 <TouchableKeypadButton
                     keyConfig={KeyConfigs.COS}
                     borders={BorderStyles.BOTTOM}
                 />
-                {/* 新增一行 */}
                 <TouchableKeypadButton
                     keyConfig={KeyConfigs.COS}
                     borders={BorderStyles.NONE}
                 />
-                {/* 新增一行 */}
                 <TouchableKeypadButton
                     keyConfig={KeyConfigs.COS}
                     borders={BorderStyles.NONE}
